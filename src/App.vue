51d1dd05ae14bd99b16fe21101bf4053ad382c68
Emman Evangelista <emmanuel.evangelista@obf.ateneo.edu>
2022-03-12 23:30:55 +0800

    add font styles

--- src/App.vue
+++ src/App.vue
@@ -7,3 +7,40 @@ export default {
   name: "App",
 };
 </script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,500;0,700;0,800;0,900;1,400&display=swap");
+
+html,
+body {
+  margin: 0;
+}
+
+*,
+.bodyText {
+  font-family: "Montserrat", Arial, sans-serif;
+  font-size: 24px;
+}
+
+.h1 {
+  font-size: 91.55px;
+}
+.h2 {
+  font-size: 73.24px;
+}
+.h3 {
+  font-size: 58.59px;
+}
+.h4 {
+  font-size: 46.88px;
+}
+.h5 {
+  font-size: 37.5px;
+}
+.h6 {
+  font-size: 30px;
+}
+.captionText {
+  font-size: 19.2px;
+}
+</style>
