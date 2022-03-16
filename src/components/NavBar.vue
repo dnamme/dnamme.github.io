94d0b78574f7c539edb62ab98294b94f9ea2916b
Emman Evangelista <emmanuel.evangelista@obf.ateneo.edu>
2022-03-16 17:25:46 +0800

    add navbar minimize on scroll and on hover

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav>
+  <nav ref="nav">
     <div id="icon" />
     <a href="#about">About</a>
     <a href="#projects">Projects</a>
@@ -12,6 +12,21 @@
 <script>
 export default {
   name: "NavBar",
+  created() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  unmounted() {
+    window.removeEventListener("scroll", this.handleScroll);
+  },
+  methods: {
+    handleScroll() {
+      if (window.scrollY <= window.innerHeight) {
+        this.$refs.nav.style.right = "0";
+      } else {
+        this.$refs.nav.style.right = "calc(-100vw + 128px + 48px)";
+      }
+    },
+  },
 };
 </script>
 
@@ -20,6 +35,7 @@ nav {
   position: fixed;
   top: 0;
   right: 0;
+  z-index: 10;
 
   width: 100%;
   height: 100px;
@@ -31,6 +47,13 @@ nav {
   flex-flow: row nowrap;
   align-items: center;
   justify-content: flex-end;
+
+  transition: right 0.5s, background-color 0.5s;
+}
+
+nav:hover {
+  right: 0 !important;
+  background-color: #fafafc;
 }
 
 nav > *:not(:nth-child(1)) {
@@ -47,5 +70,7 @@ nav > *:not(:nth-child(1)) {
   background-color: #c4c4c4;
 
   margin-right: auto;
+
+  clip-path: circle(50%);
 }
 </style>
