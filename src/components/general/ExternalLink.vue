fc250589ffe50bfc2af7e09ab43b91e6f4453bfd
Emman Evangelista <emmanuel.evangelista@obf.ateneo.edu>
2022-03-18 04:16:56 +0800

    add external link size 32

--- src/components/general/ExternalLink.vue
+++ src/components/general/ExternalLink.vue
@@ -1,5 +1,6 @@
 <template>
   <svg
+    v-if="size == 16"
     width="16"
     height="16"
     viewBox="0 0 16 16"
@@ -14,6 +15,22 @@
       stroke-linejoin="round"
     />
   </svg>
+  <svg
+    v-if="size == 32"
+    width="32"
+    height="32"
+    viewBox="0 0 32 32"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M21.5 3.5H28.5V10.5M16.5 5.5H5.5V26.5H26.5V15.5L16.5 5.5ZM26.5 5.5L15.5 16.5L26.5 5.5Z"
+      :stroke="`${color}`"
+      stroke-width="3"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
 </template>
 
 <script>
@@ -23,6 +40,10 @@ export default {
     color: {
       type: String,
       default: "black",
+    },
+    size: {
+      type: Number,
+      default: 16,
     },
   },
 };
