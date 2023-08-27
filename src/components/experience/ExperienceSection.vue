388af03db36ae393886fade6d700d971a96cdffb
Emman Evangelista <emmanuel.evangelista@obf.ateneo.edu>
2023-08-27 15:02:25 +0800

    add azeus in experiences

--- src/components/experience/ExperienceSection.vue
+++ src/components/experience/ExperienceSection.vue
@@ -40,6 +40,13 @@ export default {
     return {
       experiences: [
         {
+          title: "Web Developer Intern",
+          company: "Azeus Systems Philippine Limited",
+          company_website: "https://azeus.com",
+          date: "June 2023 to August 2023",
+          desc: "Created a demo framework for clients that enables them to track their ESG.",
+        },
+        {
           title: "Design Executive Editor",
           company: "The GUIDON",
           company_website: "https://theguidon.com",
